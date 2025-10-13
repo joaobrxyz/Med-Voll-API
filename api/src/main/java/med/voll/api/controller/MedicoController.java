98b89d3e177344536d4dc5284ea34e6ad4efe553
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-10-13 20:07:00 -0300

    Aula 21
    Lidando com erros na API

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -56,7 +56,7 @@ public class MedicoController {
     @GetMapping("/{id}")
     public ResponseEntity detalhar(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
-        
+
         return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 }
