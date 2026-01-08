8c1b4c61a5eda06ffa002ab35f9e618781fe2583
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-01-07 21:16:26 -0300

    Aula 45
    Validando cancelamento de consultas

--- api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 
-    @Query("SELECT m FROM Medico m WHERE m.ativo = true AND m.especialidade = :especialidade AND m.id not in(SELECT c.medico.id FROM Consulta c WHERE c.data = :data) ORDER BY RAND() LIMIT 1")
+    @Query("SELECT m FROM Medico m WHERE m.ativo = true AND m.especialidade = :especialidade AND m.id not in(SELECT c.medico.id FROM Consulta c WHERE c.data = :data AND c.motivoCancelamento is null) ORDER BY RAND() LIMIT 1")
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
 
     @Query("SELECT m.ativo FROM Medico m WHERE m.id = :idMedico")
